2eb5f54b2cba51f9289c3627cda3a0b7e51e23f6
aimeedyki <akuwudike.a@gmail.com>
2018-07-30 15:57:01 +0100

    feat(feeds): rename feeds page

--- realtime_feed_ui/app/containers/App/index.js
+++ realtime_feed_ui/app/containers/App/index.js
@@ -16,7 +16,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import FeedPage from '../FeedsPage/index';
+import FeedPage from '../Feeds/index';
 import AddFeedPage from '../AddFeedPage/index';
 
 export default function App() {
